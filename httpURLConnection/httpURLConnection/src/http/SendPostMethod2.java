a1c020f7c95a04d69e1fe11636d6a5b6bdb70693
송원석 <sowen2819@gmail.com>
2019-04-14 00:19:44 +0900

    change catch Block

--- httpURLConnection/httpURLConnection/src/http/SendPostMethod2.java
+++ httpURLConnection/httpURLConnection/src/http/SendPostMethod2.java
@@ -13,19 +13,20 @@ import java.nio.charset.Charset;
 public class SendPostMethod2 {
 	
 	
-	public String sendPost(String url, String body) {
+	public String sendPost(String url, String body) throws MalformedURLException, IOException {
     	
-    	String result = null;
-    	HttpURLConnection conn=null;
-    
+    	
+    	
+    	//urlSetting
     	URL urlObject = null;
 		try {
 			urlObject = new URL(url);
 		} catch (MalformedURLException e) {
-			System.err.println("new URLObejct Error");
-			e.printStackTrace();
+			throw e;
 		}
-    		
+		
+		//openConnection 생성
+    	HttpURLConnection conn=null;
     	try {
 			conn = (HttpURLConnection)urlObject.openConnection();
 		} catch (IOException e) {
@@ -36,13 +37,13 @@ public class SendPostMethod2 {
 	        	conn.disconnect();	
 	        }
 		}
-    	
-    	//==============================================================================
+		
+    	//connection Setting(변하는 부분)
     	try {
 			conn.setRequestMethod("POST");
 		} catch (ProtocolException e) {
-			System.err.println("conn setRequestMethod Error");
 			e.printStackTrace();
+			throw new IOException();
 		}finally {
 	        if(conn!=null) {
 	        	conn.disconnect();	
@@ -55,11 +56,13 @@ public class SendPostMethod2 {
     	conn.setReadTimeout(5000);
     	conn.setDoInput(true);  // Server 통신에서 입력 가능한 상태 (default = true)
     	conn.setDoOutput(true); // Server 통신에서 출력 가능한 상태 (default = true)
-    	//==============================================================================
     	
     	DataOutputStream wr=null;
         try {
 			wr = new DataOutputStream(conn.getOutputStream());
+			wr.writeBytes(body);
+			wr.flush();
+			wr.close();
 		} catch (IOException e) {
 			System.err.println("new DataOutputStream Error");
 			e.printStackTrace();
@@ -75,58 +78,6 @@ public class SendPostMethod2 {
 	        	conn.disconnect();	
 	        }
 		}
-    	try {
-			wr.writeBytes(body);
-		} catch (IOException e) {
-			System.err.println("DataOutputStream writeBytes Error");
-			e.printStackTrace();
-		}finally {
-			if(wr!=null) {
-				try {
-					wr.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}finally {
-			        if(conn!=null) {
-			        	conn.disconnect();	
-			        }
-				}
-			}
-	        if(conn!=null) {
-	        	conn.disconnect();	
-	        }
-		}
-		try {
-			wr.flush();
-		} catch (IOException e) {
-			System.err.println("DataOutputStream flush Error");
-			e.printStackTrace();
-		}finally {
-			if(wr!=null) {
-				try {
-					wr.close();
-				} catch (IOException e) {
-					e.printStackTrace();
-				}finally {
-			        if(conn!=null) {
-			        	conn.disconnect();	
-			        }
-				}
-			}
-	        if(conn!=null) {
-	        	conn.disconnect();	
-	        }
-		}	
-		try {
-			wr.close();
-		} catch (IOException e) {
-			System.err.println("DataOutputStream close Error");
-			e.printStackTrace();
-		}finally {
-	        if(conn!=null) {
-	        	conn.disconnect();	
-	        }
-		}
 		
         // Status Code Definitions Http상태코드 상태코드에 따른
         // https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
@@ -134,47 +85,31 @@ public class SendPostMethod2 {
 		try {
 			status = conn.getResponseCode();
 		} catch (IOException e) {
-			System.err.println("HttpURLConnection getResponseCode Error");
 			e.printStackTrace();
+			throw e;
 		}finally {
 	        if(conn!=null) {
 	        	conn.disconnect();	
 	        }
 		}
+		
+		//read Data
+    	String result = null;
         if(status == HttpURLConnection.HTTP_OK || status == HttpURLConnection.HTTP_CREATED || status == HttpURLConnection.HTTP_ACCEPTED){
             BufferedReader br = null;
             StringBuilder sb = null;
 			Charset charset = Charset.forName("UTF-8");
 			try {
 				br = new BufferedReader(new InputStreamReader(conn.getInputStream(), charset));
-			} catch (IOException e) {
-				System.err.println("new BufferedReader getResponseCode Error");
-				e.printStackTrace();
-			}finally {
-				if(br!=null) {
-					try {
-						br.close();
-					} catch (IOException e) {
-						e.printStackTrace();
-					} finally {
-				        if(conn!=null) {
-				        	conn.disconnect();	
-				        }
-					}
-				}
-		        if(conn!=null) {
-		        	conn.disconnect();	
-		        }
-			}
-			sb = new StringBuilder();
-			String line;
-			try {
+				sb = new StringBuilder();
+				String line;
 				while((line =br.readLine())!=null){
 				    sb.append(line+"\n");
 				}
+				br.close();
+				result = sb.toString();
 			} catch (IOException e) {
-				System.err.println("BufferedReader readLine Error");
-				e.printStackTrace();
+				throw e;
 			}finally {
 				if(br!=null) {
 					try {
@@ -191,22 +126,11 @@ public class SendPostMethod2 {
 		        	conn.disconnect();	
 		        }
 			}
-			try {
-				br.close();
-			} catch (IOException e) {
-				System.err.println("BufferedReader close Error");
-				e.printStackTrace();
-			}finally {
-		        if(conn!=null) {
-		        	conn.disconnect();	
-		        }
-			}
-			result = sb.toString();
+	
         }
-        if(conn!=null) {
-        	conn.disconnect();	
-        }
-    	
+
+        conn.disconnect();	
+
     	return result;
     	
     }
