f9f3f1b31117f158bf7df047af8be57425ad07a0
Owen Song <sowen2819@gmail.com>
2019-05-11 13:40:36 +0900

    add fileReadWrite

---
+++ fileReadWrite/src/read/Reader.java
@@ -0,0 +1,61 @@
+package read;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+	
+	public static void readFile(String fileName) {
+		
+		String data;
+		StringBuffer sb = new StringBuffer();
+		BufferedReader br = null;
+		
+		try {
+			br = new BufferedReader(new FileReader(new File(fileName)));
+		} catch (FileNotFoundException e) {
+			System.err.println("파일이 존재하지 않습니다.");
+		}
+		
+		try {
+			while((data = br.readLine()) != null) {
+				sb.append(data);
+			}
+		} catch (IOException e1) {
+			System.err.println("파일을 읽는 도중 오류가 발생했습니다."); 
+		}finally {
+		
+			try {
+				if(br!=null) {
+					br.close();	
+				}
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
+		}
+		System.out.println(sb.toString());
+				
+	}
+	
+	public static void main(String[] args) {
+		readFile("C:/test/test.txt");
+		
+//		자바는 기본적으로 모든 문자집합에 대하여 16비트로 구성된 유니코드는 사용한다		
+
+//		 finally 안에 return을 할 경우, return이 정상 종료를 의미 하므로 try 구문에서 발생한 
+//		 Exception이 출력이 되지 않습니다.
+//
+//		 finally 안에 또 다른 try catch문
+//
+//		 이 부분도 생각 많이 해야합니다. finally 구문에서 생기는 Exception이 그 전에 생긴
+//		 Exception을 잡아 먹어버림... ㅎㅎ 그래서 try 안에서 발생한 부분을 파악 할 수 없음
+
+
+
+	}
+
+}
---
+++ fileReadWrite/src/write/Writer.java
@@ -0,0 +1,41 @@
+package write;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Writer {
+
+	public static void writeFile(String fileName) {
+	
+		BufferedWriter bw = null;
+		String contents = "입력내용";
+		
+		try {
+			bw = new BufferedWriter(new FileWriter(new File(fileName)));
+		} catch (IOException e) {
+			System.out.println("파일을 읽는데 실패하였습니다.");
+		}
+		try {
+			bw.write(contents);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if(bw!=null) {
+				try {
+					bw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		writeFile("C:/test/test.txt");
+
+	}
+
+}
